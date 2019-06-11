858af516adc5e9b52e5a035cf84c051baff04d16
xiaobebe <wyy.xb@qq.com>
2019-06-11 14:53:26 +0800

    fix: add spec

---
+++ src/spec/plugin.spec.ts
@@ -0,0 +1,50 @@
+import test from 'ava';
+import { Hook, Inject, Plugin, PluginTarget } from '../index';
+
+class TestTarget extends PluginTarget {
+  @Hook
+  public method1() {
+    return 'result';
+  }
+
+  @Hook
+  public methodPromise() {
+    return new Promise(resolve => {
+      setTimeout(() => resolve('result'), 1000);
+    });
+  }
+}
+const pluginTarget = new TestTarget();
+
+// tslint:disable-next-line:max-classes-per-file
+class TestPlugin extends Plugin {
+  @Inject
+  public method1(next) {
+    return `plugin_${next()}`;
+  }
+
+  @Inject
+  public async methodPromise(next) {
+    return `plugin_${await next()}`;
+  }
+}
+
+test('add hook to plugin target', t => {
+  t.true(pluginTarget.method1.hasOwnProperty('addHook'));
+  t.true(pluginTarget.method1.hasOwnProperty('removeHook'));
+});
+
+test.serial('install method', t => {
+  const plugin = new TestPlugin();
+  pluginTarget.install(plugin);
+
+  t.is(pluginTarget.plugins.length, 1);
+});
+
+test('invoke method', t => {
+  t.is(pluginTarget.method1(), 'plugin_result');
+});
+
+test('invoke promise method', async t => {
+  t.is(await pluginTarget.methodPromise(), 'plugin_result');
+});
