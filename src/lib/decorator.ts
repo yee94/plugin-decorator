930bf8e0e30c971c4fd99ddfe0fda3b19f354afc
yee.wang <yee.wang@lazada.com>
2019-06-14 15:56:43 +0800

    fix: fix fn scope

--- src/lib/decorator.ts
+++ src/lib/decorator.ts
@@ -22,7 +22,7 @@ export function Hook(target, name, descriptor: PropertyDescriptor) {
         newArgs.splice(0, 0, next);
       }
 
-      return fn(...newArgs);
+      return fn.apply(target, newArgs);
     };
 
     const result = next();
