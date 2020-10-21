e7b6dec3c5ce3e3f98a313f3b182128f2df440c0
yee.wang <yee.wang@lazada.com>
2020-10-21 11:32:54 +0800

    feat: update main instance

--- src/lib/Plugin.ts
+++ src/lib/Plugin.ts
@@ -1,10 +1,15 @@
+import PluginTarget from './PluginTarget';
+
 export default class Plugin {
   public static pluginName: string = '';
   public static pluginSort: number;
+  public static pluginSwitch?: (options: any) => boolean;
 
   public readonly pluginHooks: string[]; // 不用指定，通过Inject自动添加
   public defaultOptions: any = {};
   public options: any = {};
+
+  public mainInstance!: PluginTarget;
 
   constructor(options = {}) {
     this.options = Object.assign({}, this.defaultOptions, options);
